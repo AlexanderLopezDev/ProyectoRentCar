15d10e98b205bb49052db79a1baee08f5c78e95a
euryteja356-alt <euryteja356@gmail.com>
2026-03-23 22:24:38 -0400

    Add files via upload
    Broder yo estoy teniendo porblema con relacion a a las imagenes jlabel

---
+++ SistemaLogin/src/main/java/com/mycompany/sistemalogin/SistemaLogin.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.sistemalogin;
+
+/**
+ *
+ * @author PC
+ */
+public class SistemaLogin {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
